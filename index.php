8848354dfe8f580f8359539666d69c5fef5e858c
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2023-11-20 01:15:16 -0300

    Create project
    Estrutura básica de organização criada, arquivos iniciais criadas, e HTML da index iniciado.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar System 1.0</title>
+</head>
+<body>
+    <header>
+        <h1>Calendar System 1.0</h1>
+        <nav>
+            <a href="#">Home</a> |
+            <a href="#">Contact</a> |
+            <a href="#">Tasks</a> |
+            <a href="#">Events</a> 
+        </nav>
+    </header>
+    <main>
+    <?php
+
+    ?>
+    </main>    
+</body>
+</html>
---
+++ pages/contacts/contacts.php
@@ -0,0 +1,1 @@
+<h3>Contacts Page</h3>
